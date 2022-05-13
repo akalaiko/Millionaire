e5a774a5f6a859e2c3d777d48e3f476ee51e0f40
kalaiko <a.kalayko@gmail.com>
2022-05-14 01:52:39 +0300

    initial layout complete

---
+++ WhoWantsToBeAMillionaire/MainMenu.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhoWantsToBeAMillionaire
+//
+//  Created by Tim on 13.05.2022.
+//
+
+import UIKit
+
+class MainMenu: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
